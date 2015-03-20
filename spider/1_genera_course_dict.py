USTC-iCourse/ustc-course

commit 31f92ea7757b5e59631521411773baeea4dce990

move course data directory; prompt user to change JSESSIONID if cookie expired

spider/2_spider002_scrap_term_courses.py
@@ -7,9 +7,10 @@
 
 import requests
 import pickle
+import os
 
 # You should change cookies["JESSIONID"] everytime you signed in
-my_cookies = {"pgv_pvi":"7886302208", "__utma": "63887786.357939715.1420618208.1421380394.1422108680.4", " __utmz": "63887786.1425197280.5.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)", "_gscu_1103646635": "20618766mew2xs15", "_ga": "GA1.3.2036932779.1422282491", "JSESSIONID": "9863C5650F4678B3A84DC1E9DEF4B914"}
+my_cookies = {"JSESSIONID": "D1904F071AB249CB534B9CCBA5FF7450"}
 term = input('2014秋季学期: 20141\n2015春季学期: 20142\n2015夏季学期: 20143\n请输入开课学期: ')
 
 try:
@@ -22,8 +23,11 @@ try:
         url = "http://mis.teach.ustc.edu.cn/querystubykcbjh.do?tag=gc&xnxq=" + term + "&kcbjh=" + key + "&kczw=" + course_dict[key][1]
 
         page = requests.get(url, cookies=my_cookies)
+        if not '学号' in page.text:
+            raise IOError('请登录综合教务系统，获取 cookie 填入代码中的 JSESSIONID 一栏')
 
-        with open("../"+term+"/"+ key +".html", "w") as newfile:
+        os.makedirs('../data/' + term, 0o755, exist_ok=True)
+        with open("../data/"+term+"/"+ key +".html", "w") as newfile:
             print(page.text, file = newfile)
 
 except IOError as err:

spider/3_handle_every_course.py
@@ -21,7 +21,7 @@ try:
 # 开始读取文件，处理信息
 ######################
     for key in course_dict:
-        data = open('../'+term+'/'+key+'.html')
+        data = open('../data/'+term+'/'+key+'.html')
         student_list = []  #这是每门课的学生名单列表
         new_student = []  #每个学生的信息是一个列表
 
@@ -59,7 +59,8 @@ try:
     for key in course_student_dict:
         print('课程号：', key)
         print('学生人数：', len(course_student_dict[key]))
-        print('第一个学生：', course_student_dict[key][0])
+        if 0 in course_student_dict[key]:
+            print('第一个学生：', course_student_dict[key][0])
         break
 
 

commit 112efe53c3ec1bbf0361a5a8f901bb9317e9243f

restore spider/ from commit 31f92ea7757b5e59631521411773baeea4dce990
accidentally removed by merge commit 4d1d9271ee73ee8173afe0c3671e5b1b0c0f8009

spider/1_genera_course_dict.py
@@ -1,0 +1,61 @@
+"""
+脚本运行顺序：１（从１开始）
+所做的操作是：把课程信息从html中剥离出来，并且打包成课程字典的pickle文件
+课程字典的格式是：key=课堂号，value=课程信息列表，列表格式是[kcid（课程id）,课程名称,学分,周学时,教师,上课班级,地点:时间,起止周]
+"""
+
+
+import pickle
+import requests
+import os
+
+term = input('2014秋季学期: 20141\n2015春季学期: 20142\n2015夏季学期: 20143\n请输入开课学期: ')
+
+#开始处理
+############################
+try:
+    print('Sending request to 综合教务系统')
+    r = requests.post("http://mis.teach.ustc.edu.cn/kbcx.do", data={"xq": "20142", "xklb": "all", "cxfs": 0})
+    print('Processing Data...')
+    omit_lines = 818
+    data_iter = iter(r.text.split('\n')[omit_lines: ])
+
+    course_dict = {}  #一学期所有的课程是一个大字典, 每个课程的value是一个列表
+
+    while next(data_iter).find('align="center" width="5%">') != -1:
+        course_key = next(data_iter).split(">")[1].split("</td")[0].strip()
+        course_dict[course_key] = []  #每个课程的value是一个列表
+        #列表格式是[kcid（课程id）,课程名称,学分,周学时,教师,上课班级,地点:时间,起止周]
+        newline = next(data_iter)
+        #课程链接这一项因为格式不对，需要特殊处理
+        course_dict[course_key].append(newline.split('kcid=')[1].split('" target')[0])
+        course_dict[course_key].append(newline.split('color="blue">')[1].split('</font>')[0])
+        for j in range(6):
+            newline = next(data_iter)
+            newline = newline.replace('&nbsp;', '')
+            newline = newline.replace('<br/>', ';')
+            course_dict[course_key].append(newline.split(">")[1].split("</td")[0])
+        for j in range(3):
+            next(data_iter)
+
+    os.makedirs('../data', 0o755, exist_ok=True)
+    with open('../data/'+term+'_course_dict.pickle', 'wb') as course_dict_file:
+        pickle.dump(course_dict, course_dict_file)
+
+
+#debug info
+    print(course_dict)
+    print("Detail: ")
+    print("Term:", term)
+    print("len(course_dict):", str(len(course_dict)))
+    print("Format:","'课堂号': ['课程id', '课程名称', '学分', '周学时', '教师', '上课班级', '地点：时间', '起止周']")
+
+
+except IOError as err:
+    print('File error: ' + str(err))
+
+except pickle.PickleError as perr:
+    print('Pickling error: ' + str(perr))
+
+except ValueError as verr:
+    print('Value error: ' + str(verr))

spider/2_spider002_scrap_term_courses.py
@@ -1,0 +1,40 @@
+"""
+脚本运行顺序：2
+这个脚本操作对象是：打包成pickle的课程字典文件
+所做的操作是：根据课程字典的key，生成url，从教务系统里抓取所有课程页面的html，需要登录。
+课程字典的格式是：key=课堂号，value=课程信息列表，列表格式是[kcid（课程id）,课程名称,学分,周学时,教师,上课班级,地点:时间,起止周]
+"""
+
+import requests
+import pickle
+import os
+
+# You should change cookies["JESSIONID"] everytime you signed in
+my_cookies = {"JSESSIONID": "D1904F071AB249CB534B9CCBA5FF7450"}
+term = input('2014秋季学期: 20141\n2015春季学期: 20142\n2015夏季学期: 20143\n请输入开课学期: ')
+
+try:
+    with open('../data/'+term+'_course_dict.pickle', 'rb') as myrestoredata:
+        course_dict = pickle.load(myrestoredata)
+
+    for key in course_dict:
+        #URL 的生成与数据的具体格式有关,
+        #value [kcid（课程id）,课程名称,学分,周学时,教师,上课班级,地点:时间,起止周]
+        url = "http://mis.teach.ustc.edu.cn/querystubykcbjh.do?tag=gc&xnxq=" + term + "&kcbjh=" + key + "&kczw=" + course_dict[key][1]
+
+        page = requests.get(url, cookies=my_cookies)
+        if not '学号' in page.text:
+            raise IOError('请登录综合教务系统，获取 cookie 填入代码中的 JSESSIONID 一栏')
+
+        os.makedirs('../data/' + term, 0o755, exist_ok=True)
+        with open("../data/"+term+"/"+ key +".html", "w") as newfile:
+            print(page.text, file = newfile)
+
+except IOError as err:
+    print('File error: ' + str(err))
+
+except pickle.PickleError as perr:
+    print('Pickling error: ' + str(perr))
+
+except ValueError as verr:
+    print('Value error: ' + str(verr))

spider/3_handle_every_course.py
@@ -1,0 +1,78 @@
+"""
+脚本运行顺序：3
+这个脚本操作的对象是：每个抓取下来的课程信息html文件（包含选课学生）
+所做的操作是：做成一个课程学生大字典，每门课是一个元素，课程是key，value是学生和学生信息列表。
+输出：一学期所有课程的大字典pickle文件。
+"""
+
+import pickle
+
+term = input('2014秋季学期: 20141\n2015春季学期: 20142\n2015夏季学期: 20143\n请输入开课学期: ')
+
+try:
+    with open('../data/'+term+'_course_dict.pickle', 'rb') as myrestoredata:
+       course_dict = pickle.load(myrestoredata)
+
+    course_student_dict = {}   #课程学生大字典，课程是key，学生名单是value
+
+
+
+#######################
+# 开始读取文件，处理信息
+######################
+    for key in course_dict:
+        data = open('../data/'+term+'/'+key+'.html')
+        student_list = []  #这是每门课的学生名单列表
+        new_student = []  #每个学生的信息是一个列表
+
+        for i in range(65):
+            data.readline()
+
+        while data.readline().find('align="center" width="10%">') != -1:
+            for j in range(4):
+                new_student.append(data.readline().split(">")[1].split("<")[0])
+            for j in range(5):
+                data.readline()
+
+            student_list.append(new_student)
+            new_student = []
+
+        data.close()
+        course_student_dict[key] = student_list
+
+
+
+######################
+#处理完毕，写入pickle
+######################
+    with open('../data/'+term+'_course_student_dict.pickle', 'wb') as course_student_file:
+        pickle.dump(course_student_dict, course_student_file)
+
+
+
+#############
+# Debug info
+#############
+    print("详细信息： ")
+    print("学期：", term)
+    print("课程总数：", str(len(course_student_dict)))
+    for key in course_student_dict:
+        print('课程号：', key)
+        print('学生人数：', len(course_student_dict[key]))
+        if 0 in course_student_dict[key]:
+            print('第一个学生：', course_student_dict[key][0])
+        break
+
+
+
+###########
+# 错误处理
+###########
+except IOError as err:
+    print('File error: ' + str(err))
+
+except pickle.PickleError as perr:
+    print('Pickling error: ' + str(perr))
+
+except ValueError as verr:
+    print('Value error: ' + str(verr))

spider/4_handle_student_id.py
@@ -1,0 +1,79 @@
+"""
+脚本运行顺序：4
+这个脚本操作对象：课程学生大字典pickle文件
+所做操作：把每个学生从所有课程中剥离出来，获得学生大字典，key是学生学号，value是学生本学期的所有课程
+"""
+
+import pickle
+
+term = input('2014秋季学期: 20141\n2015春季学期: 20142\n2015夏季学期: 20143\n请输入开课学期: ')
+
+
+try:
+    # 从每学期“课程学生”文件中导入课程学生数据，是一个大字典
+    with open('../data/'+term+'_course_student_dict.pickle', 'rb') as course_student_file:
+        course_student_dict = pickle.load(course_student_file)
+
+    # 建立一个学生字典，key是学生学号，value是学生信息（包括姓名，院系，这学期所上的课程）
+    all_student_dict = {}
+
+    # 建立一个学生姓名学号对应的列表，格式是['张静宁', 'PB14203209', '苏晓文', 'PB14000537' ]
+    # 为了方便以学生姓名搜索
+    student_name_id = []
+
+
+
+
+    # 根据课程信息列表，把所有课程学生扫一遍，建立all_student_dict字典
+    for key in course_student_dict:
+        #key是课堂号
+
+        for each_student in course_student_dict[key]:
+            # each_student 是一个带有学生信息的列表
+            # 像这样 ['PB14001067', '刘传奇', '数学科学学院', '计划必修']
+            # 所以 each_student[0] 才是 学生key
+            if each_student[0] not in all_student_dict:
+                all_student_dict[each_student[0]] = []
+                all_student_dict[each_student[0]].append(each_student[1])      #value[0] 是学生姓名
+                all_student_dict[each_student[0]].append(each_student[2])      #value[1] 是学生院系
+                all_student_dict[each_student[0]].append([])                   #value[2] 是学生一学期内所有课程信息，是个列表
+
+                # 把这个学生加到姓名学号列表上
+                student_name_id.append(each_student[1])
+                student_name_id.append(each_student[0])
+
+            # 不管学生在不在字典中，每门课程信息都应该登记一次，登记的格式是 ['多变量微积分', '计划必修']
+            all_student_dict[each_student[0]][2].append([key, each_student[3]])
+
+
+
+
+    with open("../data/"+term+"_all_student_dict.pickle", "wb") as all_student_file:
+        pickle.dump(all_student_dict, all_student_file)
+    with open("../data/"+term+"_student_name_id.pickle", "wb") as student_name_file:
+        pickle.dump(student_name_id, student_name_file)
+
+
+
+
+#debug info
+    print("详细信息： ")
+    print("学期：", term)
+    print("学生总数：", str(len(all_student_dict)))
+    for key in all_student_dict:
+        print('学生学号：', key)
+        print('学生信息：', all_student_dict[key])
+        print('本学期课程数:', len(all_student_dict[key][2]))
+        break
+
+
+
+
+except IOError as err:
+    print('File error: ' + str(err))
+
+except pickle.PickleError as perr:
+    print('Pickling error: ' + str(perr))
+
+except ValueError as verr:
+    print('Value error: ' + str(verr))