90c3aa3d9af1292b4743f37ab1cb885aed6f708d
jenny42 <jenny42@mail.ustc.edu.cn>
2015-03-26 18:32:27 +0800

    修改profile.html

--- app/views/user.py
+++ app/views/user.py
@@ -6,7 +6,7 @@ from app.ext.login import login_uesr
 
 user = Blueprint('user', __name__)
 
-@user.route('/1')
+@user.route('/1/')
 def view_profile():
     '''用户的个人主页,展示用户在站点的活跃情况'''
 
@@ -15,6 +15,7 @@ def view_profile():
 @user.route('/1/settings/')
 def account_settings():
     '''账户设置,包括改密码等'''
+
     return render_template('settings.html')
 
 
