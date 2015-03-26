b64c6854b4817107d28d0119ffc748d1f379b93f
jenny42 <jenny42@mail.ustc.edu.cn>
2015-03-26 13:03:53 +0800

    增加用户个人主页和账户设置的链接,未完成

--- app/views/user.py
+++ app/views/user.py
@@ -6,5 +6,15 @@ from app.ext.login import login_uesr
 
 user = Blueprint('user', __name__)
 
+@user.route('/1')
+def view_profile():
+    '''用户的个人主页,展示用户在站点的活跃情况'''
+
+    return render_template('profile.html')
+
+@user.route('/1/settings/')
+def account_settings():
+    '''账户设置,包括改密码等'''
+    return render_template('settings.html')
 
 
