a584240c4e65474b5cee2702a43bd5dc04b45e87
Bojie Li <bojieli@gmail.com>
2015-04-15 05:33:47 +0000

    fix image model

--- app/models/image.py
+++ app/models/image.py
@@ -1,5 +1,9 @@
 from app import db
 from datetime import datetime
+try:
+    from flask.ext.login import current_user
+except:
+    current_user=None
 
 class ImageStore(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
@@ -8,8 +12,9 @@ class ImageStore(db.Model):
     upload_time = db.Column(db.DateTime(), default=datetime.utcnow)
     stored_filename = db.Column(db.String(80), unique=True)
 
-    def __init__(self, author, filename, stored_filename):
-        self.author = author
-        self.filename = filename
-        self.upload_time = datetime.now()
-        self.stored_filename = stored_filename
+    def __init__(self, filename, stored_filename, author=current_user):
+        if author:
+            self.author_id = author.id
+            self.filename = filename
+            self.upload_time = datetime.now()
+            self.stored_filename = stored_filename
