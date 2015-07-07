ab24c1af5b8d899e7fbafb221aa1614db17c4dd6
Bojie Li <bojieli@gmail.com>
2015-07-07 21:22:26 +0000

    script to regenerate notifications

---
+++ tests/regenerate_notifications.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append('..')  # fix import directory
+
+from app import app
+from app.utils import send_confirm_mail
+from app.models import *
+
+# require an app context to work
+ctx = app.test_request_context()
+ctx.push()
+
+reviews = Review.query.order_by(Review.id).all()
+for review in reviews:
+    for user in set(review.course.followers + review.course.joined_users):
+        user.notify('review', review, review.author, ref_display_class='Course')
+    for user in review.upvote_users:
+        review.author.notify('upvote', review, user)
+    for comment in review.comments:
+        review.author.notify('comment', review, comment.author)
+
+
+users = User.query.all()
+for user in users:
+    for course in user.courses_upvoted:
+        for follower in course.followers:
+            follower.notify('upvote', course, user)
+    for course in user.courses_downvoted:
+        for follower in course.followers:
+            follower.notify('downvote', course, user)
+    for course in user.courses_following:
+        for follower in course.followers:
+            follower.notify('follow', course, user)
