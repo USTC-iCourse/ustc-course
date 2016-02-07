bba6e58bfe5127ac087bc925b04b8cf0e1f84adf
Bojie Li <bojieli@gmail.com>
2016-02-07 17:01:43 +0800

    script to regenerate notifications

--- tests/regenerate_notifications.py
+++ tests/regenerate_notifications.py
@@ -10,24 +10,29 @@ from app.models import *
 ctx = app.test_request_context()
 ctx.push()
 
-reviews = Review.query.order_by(Review.id).all()
-for review in reviews:
-    for user in set(review.course.followers + review.course.joined_users):
-        user.notify('review', review, review.author, ref_display_class='Course')
-    for user in review.upvote_users:
-        review.author.notify('upvote', review, user)
-    for comment in review.comments:
-        review.author.notify('comment', review, comment.author)
+notifications = Notification.query.all()
+for n in notifications:
+    n.save()
+    print(n.display_text)
 
-
-users = User.query.all()
-for user in users:
-    for course in user.courses_upvoted:
-        for follower in course.followers:
-            follower.notify('upvote', course, user)
-    for course in user.courses_downvoted:
-        for follower in course.followers:
-            follower.notify('downvote', course, user)
-    for course in user.courses_following:
-        for follower in course.followers:
-            follower.notify('follow', course, user)
+#reviews = Review.query.order_by(Review.id).all()
+#for review in reviews:
+#    for user in set(review.course.followers + review.course.joined_users):
+#        user.notify('review', review, review.author, ref_display_class='Course')
+#    for user in review.upvote_users:
+#        review.author.notify('upvote', review, user)
+#    for comment in review.comments:
+#        review.author.notify('comment', review, comment.author)
+#
+#
+#users = User.query.all()
+#for user in users:
+#    for course in user.courses_upvoted:
+#        for follower in course.followers:
+#            follower.notify('upvote', course, user)
+#    for course in user.courses_downvoted:
+#        for follower in course.followers:
+#            follower.notify('downvote', course, user)
+#    for course in user.courses_following:
+#        for follower in course.followers:
+#            follower.notify('follow', course, user)
