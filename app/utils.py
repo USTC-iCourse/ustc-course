0b62e82d485dce7d84225b809cb2333c46d3a600
Bojie Li <bojieli@gmail.com>
2017-01-15 22:41:34 +0800

    improve at parser: do not validate username

--- app/utils.py
+++ app/utils.py
@@ -95,26 +95,24 @@ def editor_parse_at(text):
     if not text.endswith('\n'):
         text = text + '\n' # the parse function will not work with @somebody
     mentioned_users = []
-    print(text)
-    matches = re.finditer('(@|＠)([^＠@&<>"\':;?+=,\s]+)', text)
+    matches = re.finditer('(@|＠)([^＠@<>"\'\s]+)', text)
     if not matches:
         return text, set(mentioned_users)
     for username in set([match.group(2) for match in matches]):
         if len(username) > 30:
             continue
-        if validate_username(username, check_db=False) == 'OK':
-            user = User.query.filter_by(username=username).first()
-            if user:
-                url = url_for('user.view_profile', user_id=user.id)
-                # replace @ to Unicode char ＠ to avoid further substitution when review is edited
-                atstring = '<a href="' + url + '">' + '@' + username + '</a>'
-                # warn: simple str.replace is wrong.
-                # consider the following case: @boj @bojjenny42
-                #   @boj is first matched and replaced, then the string becomes <a href="">@boj</a> <a href="">@boj</a>jenny42
-                # the following regexp would do the trick.
-                text = re.sub("(@|＠)" + re.escape(username) + '([@&<>"\':;?+=,\s])',
-                              atstring + '\\2', text)
-                mentioned_users.append(user)
+        user = User.query.filter_by(username=username).first()
+        if user:
+            url = url_for('user.view_profile', user_id=user.id)
+            # replace @ to Unicode char ＠ to avoid further substitution when review is edited
+            atstring = '<a href="' + url + '">' + '@' + username + '</a>'
+            # warn: simple str.replace is wrong.
+            # consider the following case: @boj @bojjenny42
+            #   @boj is first matched and replaced, then the string becomes <a href="">@boj</a> <a href="">@boj</a>jenny42
+            # the following regexp would do the trick.
+            text = re.sub("(@|＠)" + re.escape(username) + '([＠@<>"\'\s])',
+                          atstring + '\\2', text)
+            mentioned_users.append(user)
     return text, set(mentioned_users)
 
 @app.template_filter('localtime')
