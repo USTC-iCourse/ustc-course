2417f419d83752880e6200351a2a09be8d7c2805
Bojie Li <bojieli@gmail.com>
2017-01-15 22:29:58 +0800

    fix at parser for chinese chars

--- app/utils.py
+++ app/utils.py
@@ -95,11 +95,11 @@ def editor_parse_at(text):
     if not text.endswith('\n'):
         text = text + '\n' # the parse function will not work with @somebody
     mentioned_users = []
-    matches = re.findall('@[^@&<>"\':;?+=,\s]+', text)
+    print(text)
+    matches = re.finditer('(@|＠)([^＠@&<>"\':;?+=,\s]+)', text)
     if not matches:
         return text, set(mentioned_users)
-    for match in matches:
-        username = match[1:]
+    for username in set([match.group(2) for match in matches]):
         if len(username) > 30:
             continue
         if validate_username(username, check_db=False) == 'OK':
@@ -107,13 +107,13 @@ def editor_parse_at(text):
             if user:
                 url = url_for('user.view_profile', user_id=user.id)
                 # replace @ to Unicode char ＠ to avoid further substitution when review is edited
-                atstring = '<a href="' + url + '">' + '＠' + username + '</a>'
+                atstring = '<a href="' + url + '">' + '@' + username + '</a>'
                 # warn: simple str.replace is wrong.
                 # consider the following case: @boj @bojjenny42
                 #   @boj is first matched and replaced, then the string becomes <a href="">@boj</a> <a href="">@boj</a>jenny42
                 # the following regexp would do the trick.
-                text = re.sub("@" + re.escape(username) + '([@&<>"\':;?+=,\s])',
-                              '<a href="' + url + '">' + '＠' + re.escape(username) + '</a>' + '\\1', text)
+                text = re.sub("(@|＠)" + re.escape(username) + '([@&<>"\':;?+=,\s])',
+                              atstring + '\\2', text)
                 mentioned_users.append(user)
     return text, set(mentioned_users)
 
