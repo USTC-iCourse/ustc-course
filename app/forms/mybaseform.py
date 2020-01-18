d6fad2e9f4f6d886df9bca1edba2e03510416cae
Bojie Li <bojieli@gmail.com>
2020-01-19 00:13:37 +0800

    increase csrf time limit to 8 hours (default was 30 minutes, too low)

--- app/forms/mybaseform.py
+++ app/forms/mybaseform.py
@@ -1,4 +1,5 @@
 from wtforms.csrf.session import SessionCSRF
+from datetime import timedelta
 
 
 class MyBaseForm(FlaskForm):
@@ -6,6 +7,7 @@ class MyBaseForm(FlaskForm):
         csrf = True
         csrf_class = SessionCSRF
         csrf_secret = app.config['CSRF_SECRET_KEY']
+        csrf_time_limit = timedelta(hours=8)
 
         @property
         def csrf_context(self):
