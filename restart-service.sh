404044664ab05d270b0f05092966118248c2e02d
Bojie Li <bojieli@gmail.com>
2021-01-07 11:49:33 +0800

    script to restart service

---
+++ restart-service.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+
+getpid()
+{
+  PID=$(ps aux | grep gunicorn | awk '{print $2}' | sort | head -n 1)
+}
+
+getpid
+echo "Original PID: $PID"
+cd /srv/ustc-course && kill $PID && nohup /home/icourse/.local/bin/gunicorn -w 8 -b 127.0.0.1:3000 app:app >/dev/null 2>&1 &
+echo "Original process killed, spawning new process..."
+sleep 3
+getpid
+echo "New PID: $PID"
