f58f23387db444b41286ef78f760ecf1728329e4
Bojie Li <bojieli@gmail.com>
2022-06-18 15:13:35 +0800

    script to init review and comments history

---
+++ tests/init_review_history.py
@@ -0,0 +1,36 @@
+
+#!/usr/bin/python3
+import sys
+sys.path.append('..')
+from app import db
+from app.models import Review, ReviewComment
+from app.views.review import record_review_history
+from app.views.api import record_review_comment_history
+
+def update_reviews():
+    reviews = Review.query.all()
+    for review in reviews:
+        operation = 'create' if review.publish_time == review.update_time else 'update'
+        history = record_review_history(review, operation, commit=False)
+        history.operation_time = review.update_time
+        history.operation_user_id = review.author_id
+        db.session.add(history)
+
+    db.session.commit()
+
+
+def update_comments():
+    comments = ReviewComment.query.all()
+    for comment in comments:
+        operation = 'create'
+        history = record_review_comment_history(comment, operation, commit=False)
+        history.operation_time = comment.publish_time
+        history.operation_user_id = comment.author_id
+        db.session.add(history)
+
+    db.session.commit()
+
+
+
+update_reviews()
+update_comments()
