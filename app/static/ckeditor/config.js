12327eb27c498a3d829be6d76cec1c27d917ac06
Bojie Li <bojieli@gmail.com>
2022-07-03 18:09:26 +0800

    remove glyphicons from ckeditor because it conflicts with site CSS

--- app/static/ckeditor/config.js
+++ app/static/ckeditor/config.js
@@ -29,7 +29,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline';
+	config.removeButtons = 'Underline,Glyphicons';
 
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
