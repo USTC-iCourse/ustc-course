0e648e134c48fd0357be1cb4d98504893b5eeab1
Bojie Li <bojieli@gmail.com>
2022-11-14 01:01:32 +0800

    fix duplicate course group entries

--- tests/update_course_group_relation.py
+++ tests/update_course_group_relation.py
@@ -9,8 +9,18 @@ course_terms = CourseTerm.query.all()
 course_groups = CourseGroup.query.all()
 course_group_map = { course_group.code : course_group for course_group in course_groups }
 
+
+def is_in_group(course_term, course_group):
+    for group in course_term.course.course_groups:
+        if group == course_group:
+            return True
+    return False
+
+
 for course_term in course_terms:
     if course_term.code in course_group_map:
-        course_term.course.course_groups += [course_group_map[course_term.code]]
+        course_group = course_group_map[course_term.code]
+        if not is_in_group(course_term, course_group):
+            course_term.course.course_groups += [course_group]
 
 db.session.commit()
