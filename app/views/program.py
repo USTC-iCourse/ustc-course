57aaead0160766d5fae2e5556aba0c83d2cb46d5
Bojie Li <bojieli@gmail.com>
2022-12-26 22:07:55 +0800

    highlight selected course in course program

--- app/views/program.py
+++ app/views/program.py
@@ -14,4 +14,11 @@ def view_program(program_id):
     program = Program.query.filter_by(id=program_id).first()
     if not program:
         abort(404)
-    return render_template('program.html', program=program)
+
+    highlight_course_id = request.args.get('highlight_course')
+    if highlight_course_id:
+        highlight_course_id = int(highlight_course_id)
+    else:
+        highlight_course_id = None
+
+    return render_template('program.html', program=program, highlight_course_id=highlight_course_id)
