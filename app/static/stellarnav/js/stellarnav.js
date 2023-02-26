a5d88feeb1cbf18b8e44f623c9513c680c23383f
Bojie Li <bojieli@gmail.com>
2023-02-26 18:36:30 +0800

    implement stellarnav styles to make it inline

--- app/static/stellarnav/js/stellarnav.js
+++ app/static/stellarnav/js/stellarnav.js
@@ -77,9 +77,9 @@
 
 			// adds the toggle button to open and close nav
 			if ( settings.position == 'right' || settings.position == 'left' ) {
-				nav.prepend('<a href="#" class="menu-toggle"><span class="bars"><span></span><span></span><span></span></span> ' + menuLabel + '</a>');
-			} else {
-				nav.prepend('<a href="#" class="menu-toggle ' + cssClass + '"><span class="bars"><span></span><span></span><span></span></span> ' + menuLabel + '</a>');
+				nav.append('<a href="#" class="menu-toggle"><span class="bars"><span></span><span></span><span></span></span> ' + menuLabel + '</a>');
+			} else if (settings.position != 'none') {
+				nav.append('<a href="#" class="menu-toggle ' + cssClass + '"><span class="bars"><span></span><span></span><span></span></span> ' + menuLabel + '</a>');
 			}
 
 			// adds a click-to-call link
