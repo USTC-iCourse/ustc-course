b5b73f05a38e69ad1034f1e1b489643265354201
Bojie Li <bojieli@gmail.com>
2023-02-26 19:28:44 +0800

    program list page

--- app/views/program.py
+++ app/views/program.py
@@ -5,9 +5,25 @@ from app import db
 
 program = Blueprint('program', __name__)
 
+# key1: { key2: [value1, value2] } }
+# value1 and value2 are sorted by sort_key
+def add_to_2_level_dict(root, key1, key2, value, sort_key):
+    if key1 in root:
+        if key2 in root[key1]:
+            root[key1][key2].append(value)
+            root[key1][key2].sort(key=sort_key, reverse=True)
+        else:
+            root[key1][key2] = [value]
+    else:
+        root[key1] = {key2: [value]}
+
 @program.route('/')
 def index():
-    abort(403)
+    programs = Program.query.all()
+    program_dict = {}
+    for program in programs:
+        add_to_2_level_dict(program_dict, program.dept, program.name, program, lambda program: program.grade)
+    return render_template('program-list.html', title='培养方案列表', programs=program_dict)
 
 @program.route('/<int:program_id>/')
 def view_program(program_id):
