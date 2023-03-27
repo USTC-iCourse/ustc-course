67393eaf24e0b63b0d0a3142b46caf1deaefae3f
Bojie Li <bojieli@gmail.com>
2023-03-27 21:27:05 +0800

    connector to milvus vector database for storing embeddings

---
+++ tests/chatgpt/milvus_connector.py
@@ -0,0 +1,30 @@
+import time
+
+import numpy as np
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema, CollectionSchema, DataType,
+    Collection,
+)
+
+fmt = "\n=== {:30} ===\n"
+dim = 1536    # the dim for text-embedding-ada-002
+
+print(fmt.format("start connecting to Milvus"))
+connections.connect("default", host="hwcloud-vpc.ring0.me", port="19530")
+
+has = utility.has_collection("icourse_reviews")
+print(f"Does collection icourse_reviews exist in Milvus: {has}")
+
+fields = [
+    FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=False),
+    FieldSchema(name="course_id", dtype=DataType.INT64),
+    FieldSchema(name="author_id", dtype=DataType.INT64),
+    FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=dim)
+]
+
+schema = CollectionSchema(fields, "get the embeddings of icourse reviews")
+
+milvus_collection = Collection("icourse_reviews", schema, consistency_level="Strong")
+print(fmt.format("Created collection `icourse_reviews`"))
